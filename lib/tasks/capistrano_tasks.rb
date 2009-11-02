ce2f797478a65013f22526b3032c279c01decd8b
Seamus Abshere <seamus@abshere.net>
2009-11-02 09:00:06 -0500

    Clean up tasks.

--- lib/tasks/capistrano_tasks.rb
+++ lib/tasks/capistrano_tasks.rb
@@ -5,74 +5,65 @@ require 'activesupport'
 require 'zlib'
 
 # basically { :a => 1, :b => 2 }.eql?({ :b => 2, :a => 1}) => true
-# assumes hash elements are basically strings
-# will break if a value is an unordered hash that randomly to_s'es in different ways
-class StrictHash < Hash
+class StrictComparableHash < Hash
   def eql?(other)
     self == other
   end
-  # has to be an integer or #eql? won't be used
+
+  # must return an integer or #eql? will be ignored by Comparable
+  # assumes hash elements are strings
+  # will break if a value is something else whose to_s varies randomly
   def hash
     Zlib.crc32(to_a.map(&:to_s).sort.to_s)
   end
 end
 
-SWITCH_SHORTHANDS = {
-  'c' => 'list_current',
-  'd' => 'diff',
-  'on' => 'turn_on',
-  'off' => 'turn_off'
-}
-
-REQUIRED_CAPISTRANO_VARIABLES = %w{
-  deploy_to
-  rails_env
-  gfs
-}
+# Hash.from_xml(a.gsub(/\n+/, ''))['hash']
 
 namespace :s do
-  %w{ c d on off }.each do |switch_command|
-    task switch_command.to_sym, :roles => lambda { roles.has_key?(:app_master) ? [ :app, :app_master ] : :app } do
-      REQUIRED_CAPISTRANO_VARIABLES.each do |cvar|
-        begin; send cvar; rescue; puts "[SWITCHES GEM] Please set :#{cvar} (probably in your cap deploy script)"; raise $!; end
-      end
-      raw = Hash.new
-      switch_command = SWITCH_SHORTHANDS[switch_command] if SWITCH_SHORTHANDS.has_key?(switch_command)
-      run "cd #{deploy_to}/current; rake --silent switches:#{switch_command}#{"[#{ENV['ARG']}]" if ENV['ARG'].present?} RAILS_ENV=#{rails_env}; true" do |channel, stream, data|
-        unless data.starts_with?('Switches') #or data.starts_with?('(')
-          server = channel[:server]
-          key = gfs ? server.host : "#{server.host}:#{server.port}".chomp(':')
-          raw[key] ||= Array.new
-          raw[key] << data
-        end
+  %w{ c d on off clear reset backup restore default }.each do |cmd|
+    task cmd.to_sym, :roles => lambda { roles.keys.map(&:to_s).grep(/app/).map(&:to_sym) } do
+      
+      # download switches xml from servers
+      raw_input = Hash.new
+      run "cd #{deploy_to}/current; rake --silent s:#{cmd}#{"[#{ENV['ARG']}]" if ENV['ARG'].present?} RAILS_ENV=#{rails_env}; true" do |channel, stream, data|
+        server = channel[:server]
+        server_identifier = gfs ? server.host : "#{server.host}:#{server.port}".chomp(':')
+        raw_input[server_identifier] ||= Array.new
+        raw_input[server_identifier] << data
       end
       
+      grouped_by_server = raw_input.inject(Hash.new) do |memo, ary|
+        server_identifier, chunks = ary
+        server_hash = Hash.from_xml(chunks.join.gsub(/\n+/, ''))['hash']
+        memo[server_identifier] = server_hash.is_a?(Hash) ? StrictComparableHash[server_hash] : StrictComparableHash.new
+        memo
+      end
+      
+      first_server_switches = grouped_by_server.values.first
+
+      # If GFS, all server switches will be the same---just output them
       if gfs
-        cooked = YAML.load raw.values.first.join
-        pp cooked
+        pp first_server_switches
       else
-        sub_cooked = raw.inject(Hash.new) do |memo, ary|
-          host, chunks = ary
-          memo[host] = StrictHash[YAML.load(chunks.join)]
+        grouped_by_switches = grouped_by_server.inject(Hash.new) do |memo, ary|
+          server_identifier, comparable_switches = ary
+          memo[comparable_switches] ||= Array.new
+          memo[comparable_switches] << server_identifier
           memo
         end
-        by_recipe = sub_cooked.inject(Hash.new) do |memo, ary|
-          host, switches = ary
-          memo[switches] ||= Array.new
-          memo[switches] << host
-          memo
-        end
-        if by_recipe.keys.length > 1
-          puts "Servers have different switches"
-          pp by_recipe.invert
-          if by_recipe.length == 2
-            a, b = by_recipe.invert.values
-            puts "Difference is that #{by_recipe.values.first} has"
+        if grouped_by_switches.keys.length > 1
+          puts "Servers are different"
+          pp grouped_by_switches.invert
+          if grouped_by_switches.length == 2
+            a, b = grouped_by_switches.invert.values
+            puts
+            puts "Difference is"
             pp a.diff(b)
           end
         else
-          puts "Servers have the same switches"
-          pp sub_cooked.to_a.first.last
+          puts "Servers are the same"
+          pp first_server_switches
         end
       end
     end
--- lib/tasks/switches.rake
+++ lib/tasks/switches.rake
@@ -1,88 +1,54 @@
 require 'switches'
 
-task :switches do
-  Rake::Task['switches:default'].execute
-end
-
 namespace :s do
-  task :c => 'switches:list_current'
-  task :d => 'switches:diff'
-  task :on, :name do |t, args|
-    Rake::Task['switches:turn_on'].execute args
-  end
-  task :off, :name do |t, args|
-    Rake::Task['switches:turn_off'].execute args
-  end
-end
-
-namespace :switches do
-  desc "List current and show instructions"
-  task :default do
-    puts <<-EOS
-
-Here's a bunch of ugly instructions:
-
-List current settings:
-  rake s:c
-Show difference between current and default settings:
-  rake s:d
-Clear with:
-  rake switches:clear[FOOBAR]
-
-... now listing current settings ...
-    EOS
-    Rake::Task['switches:list_current'].execute
-  end
-  
-  # not called :default so it doesn't look like the default task
-  desc "List default"
-  task :list_default do
-    puts Switches.default.to_yaml
-  end
-  
   desc "List current"
-  task :list_current do
-    puts Switches.current.to_yaml
+  task :c do
+    puts Switches.current.to_xml
   end
-    
+
+  desc "Diff current vs. default switches"
+  task :d do
+    puts Switches.diff.to_xml
+  end
+
   desc "Turn on switch"
-  task :turn_on, :name do |t, args|
+  task :on, :name do |t, args|
     Switches.turn_on args.name
-    puts Switches.current.to_yaml
+    puts Switches.current.to_xml
   end
 
   desc "Turn off switch"
-  task :turn_off, :name do |t, args|
+  task :off, :name do |t, args|
     Switches.turn_off args.name
-    puts Switches.current.to_yaml
+    puts Switches.current.to_xml
   end
-  
+
   desc "Clear switch"
   task :clear, :name do |t, args|
     Switches.clear args.name
-    puts Switches.current.to_yaml
+    puts Switches.current.to_xml
   end
-  
-  desc "Diff current vs. default switches"
-  task :diff do
-    puts Switches.diff.to_yaml
-  end
-  
+
   desc "Reset all switches to defaults"
   task :reset do
     Switches.reset
-    puts Switches.current.to_yaml
+    puts Switches.current.to_xml
   end
-  
+
   desc "Backup all switches to defaults"
   task :backup do
     Switches.backup
-    puts Switches.current.to_yaml
+    puts Switches.current.to_xml
   end
-  
+
   desc "Restore all switches to defaults"
   task :restore do
     Switches.restore
-    puts Switches.current.to_yaml
+    puts Switches.current.to_xml
+  end
+
+  desc "List default"
+  task :default do
+    puts Switches.default.to_xml
   end
 end
